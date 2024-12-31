3870939d37edc4cdd1f02f5918db072f5cd48c38
mohammad.zolfaghari <zolfaghary71@gmail.com>
2024-12-31 21:22:39 +0330

    fix jwt bug

--- Darya.Api/Models/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Darya.Api.Models;
-
-public class JwtSettings
-{
-    public string SecretKey { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
-    public int TokenExpiryInMinutes { get; set; }
-}
